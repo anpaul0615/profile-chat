09fe2fdb2eeaf7c05a0ab004513fff84f12e4679
PAUL AN <anpaul0615@gmail.com>
2018-10-13 21:29:25 +0900

    Apply airbnb eslint to root-component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import ChatSignin from './components/ChatSignin';
 import ChatSignup from './components/ChatSignup';
@@ -7,60 +7,71 @@ import ChatGroup from './components/ChatGroup';
 import LoadingCircle from './components/LoadingCircle';
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            isPending: false,
-            currentPage: '/signin',
-            currentUser: '',
-            currentGroup: ''
-        };
-    }
+  constructor() {
+    super();
+    this.state = {
+      isPending: false,
+      currentPage: '/signin',
+    };
+  }
 
-    /* State Manage Functions */
-    getGlobalState = ()=>{
-        return new Promise((resolve) => {
-            resolve(this.state);
-        });
-    }
-    setGlobalState = (newState)=>{
-        return new Promise((resolve) => {
-            this.setState({ ...this.state, ...newState }, resolve)
-        });
-    }
+  getGlobalState = () => new Promise(
+    resolve => resolve(this.state),
+  );
 
-    render() {
-        return (
-            <div className="App">
-                { this.state.isPending ? <LoadingCircle /> : null }
-                {(()=>{
-                switch(this.state.currentPage) {
-                case '/signin':
-                    return <ChatSignin
-                                key={'ChatSignin'}
-                                setGlobalState={this.setGlobalState}
-                                getGlobalState={this.getGlobalState} />
-                case '/signup':
-                    return <ChatSignup
-                                key={'ChatSignup'}
-                                setGlobalState={this.setGlobalState}
-                                getGlobalState={this.getGlobalState} />;
-                case '/group':
-                    return <ChatGroup
-                                key={'ChatGroup'}
-                                setGlobalState={this.setGlobalState}
-                                getGlobalState={this.getGlobalState} />;
-                case '/':
-                    return <Chat
-                                key={'Chat'}
-                                setGlobalState={this.setGlobalState}
-                                getGlobalState={this.getGlobalState} />;
-                default:
-                    return <h1>Something is wrong..!</h1>;
-                }
-                })()}
-            </div>
-        );
-    }
+  setGlobalState = newState => new Promise(
+    resolve => this.setState(() => {
+      resolve();
+      return { ...newState };
+    }),
+  );
+
+  render() {
+    const { isPending, currentPage } = this.state;
+    return (
+      <div className="App">
+        { isPending && <LoadingCircle /> }
+        {(() => {
+          switch (currentPage) {
+            case '/signin':
+              return (
+                <ChatSignin
+                  key="ChatSignin"
+                  setGlobalState={this.setGlobalState}
+                  getGlobalState={this.getGlobalState}
+                />
+              );
+            case '/signup':
+              return (
+                <ChatSignup
+                  key="ChatSignup"
+                  setGlobalState={this.setGlobalState}
+                  getGlobalState={this.getGlobalState}
+                />
+              );
+            case '/group':
+              return (
+                <ChatGroup
+                  key="ChatGroup"
+                  setGlobalState={this.setGlobalState}
+                  getGlobalState={this.getGlobalState}
+                />
+              );
+            case '/':
+              return (
+                <Chat
+                  key="Chat"
+                  setGlobalState={this.setGlobalState}
+                  getGlobalState={this.getGlobalState}
+                />
+              );
+            default:
+              return <h1>Something is wrong..!</h1>;
+          }
+        })()}
+      </div>
+    );
+  }
 }
+
 export default App;
